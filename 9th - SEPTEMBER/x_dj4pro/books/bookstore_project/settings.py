f902b2174fb00b28c4cfbb614e2169366e11e374
hakimi_hamzan <hakimihamzan20@gmail.com>
2020-09-09 23:04:36 +0800

    add new page

--- 9th - SEPTEMBER/x_dj4pro/books/bookstore_project/settings.py
+++ 9th - SEPTEMBER/x_dj4pro/books/bookstore_project/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     #local
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'bookstore_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
