45ed63bb4467957de1a5b791d1c4ee1719fdf1ed
hakimi_hamzan <hakimihamzan20@gmail.com>
2020-09-20 15:01:32 +0800

    some

--- 9th - SEPTEMBER/lpthw/scr2.py
+++ 9th - SEPTEMBER/lpthw/scr2.py
@@ -1,8 +1,29 @@
+#
+# post
 import requests
 
-payload = {'username': "kimi", 'password': 'poass'}
-r = requests.post('https://httpbin.org/post', data=payload)
+payload = {'title': "yehaa", 'description': 'we win'}
+r = requests.post('http://127.0.0.1:8000/apis/v1/', data=payload)
 
-r_dict = r.json()
-print(r_dict['form'])
-print(r.url)
+print(r)
+
+
+
+#update part
+import requests
+
+payload = {'title': "yehaa", 'description': 'we win34'}
+r = requests.put('http://127.0.0.1:8000/apis/v1/18', data=payload)
+
+print(r)
+
+
+#delete
+#
+import requests
+
+r = requests.delete('http://127.0.0.1:8000/apis/v1/17')
+print(r)
+
+
+#create
