d12c773f8ca64f25cb2c8673ec32726c699a471b
Hakimi Hamzan <hakimihamzan20@gmail.com>
2020-11-05 19:19:54 +0800

    starting files for weather app

---
+++ 10th - October/x3_climate_app/notes.dart
@@ -0,0 +1,13 @@
+// this file doesnt do anything other than to keep notes
+//
+// LOCATION(add both req for android and ios to access the location)
+//
+// <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+// <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+//
+//
+// <key>NSLocationWhenInUseUsageDescription</key>
+// <string>This app needs access to location when open.</string>
+// <key>NSLocationAlwaysUsageDescription</key>
+// <string>This app needs access to location when in the background.</string>
+//
