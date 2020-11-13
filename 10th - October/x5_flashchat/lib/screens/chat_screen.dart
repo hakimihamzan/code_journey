bd9bf763c5da2b88e37a5f2c22d0d6e7a1fde111
Hakimi Hamzan <hakimihamzan20@gmail.com>
2020-11-13 17:34:41 +0800

    add dart stream to pull data realtime in chat messages

--- 10th - October/x5_flashchat/lib/screens/chat_screen.dart
+++ 10th - October/x5_flashchat/lib/screens/chat_screen.dart
@@ -33,6 +33,22 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').get();
+  //   // print(messages.documents);
+  //   for (var message in messages.docs) {
+  //     print(message.data());
+  //   }
+  // }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,8 +59,9 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.close),
               onPressed: () {
                 //Implement logout functionality
-                _auth.signOut();
-                Navigator.pop(context);
+                // _auth.signOut();
+                // Navigator.pop(context);
+                messagesStream();
               }),
         ],
         title: Text('⚡️Chat'),
