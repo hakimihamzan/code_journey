7aa179f076bccd4168f6e969767c99392ae8f245
Hakimi Hamzan <hakimihamzan20@gmail.com>
2020-11-14 16:33:09 +0800

    to do list app init

---
+++ 10th - October/x6_todo_list/lib/screens/tasks_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SafeArea(
+        child: Column(
+          children: [
+            Text('HAHAHAHAH'),
+            Expanded(
+              child: SizedBox(
+                height: 40.0,
+              ),
+            ),
+            Text("AHAHAHAHAHAH"),
+          ],
+        ),
+      ),
+    );
+  }
+}
