b4cefa294581f52cecd96e88566c79eaa53f2aab
Hakimi Hamzan <hakimihamzan20@gmail.com>
2020-11-17 15:08:02 +0800

    update set state(lift) and refactors codes

---
+++ 11th - NOVEMBER/x6_todo_list/lib/widgets/tasks_tile.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class TasksTile extends StatefulWidget {
+  @override
+  _TasksTileState createState() => _TasksTileState();
+}
+
+class _TasksTileState extends State<TasksTile> {
+  bool isChecked = false;
+
+  void checkBoxCallBack(bool checkBoxState) {
+    setState(() {
+      isChecked = checkBoxState;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        'Go to the gym.',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckbox(
+        checkBoxState: isChecked,
+        toggleCheckboxState: checkBoxCallBack,
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox({this.checkBoxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkBoxState,
+      onChanged: toggleCheckboxState,
+    );
+  }
+}
