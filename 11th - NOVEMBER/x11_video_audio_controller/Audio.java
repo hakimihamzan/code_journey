edbe5325774066aaba566d5bb3208d7586b0542f
hakimihamzan <hakimihamzan20@gmail.com>
2020-11-25 12:38:29 +0800

    messing aroung with audio player with play pause button

---
+++ 11th - NOVEMBER/x11_video_audio_controller/Audio.java
@@ -0,0 +1,37 @@
+package com.k31.audioplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+
+    public void play(View view) {
+
+        mediaPlayer.start();
+
+    }
+
+    public void pause(View view) {
+
+        mediaPlayer.pause();
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.illeniumfractures);
+
+
+
+
+    }
+}
