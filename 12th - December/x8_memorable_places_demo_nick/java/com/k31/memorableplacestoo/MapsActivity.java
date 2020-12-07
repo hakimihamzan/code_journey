a5f8893e6807b5baa73fda6437efc2a02cb8c89c
hakimihamzan <hakimihamzan20@gmail.com>
2020-12-07 23:10:23 +0800

    complete with shared preference

--- 12th - December/x8_memorable_places_demo_nick/java/com/k31/memorableplacestoo/MapsActivity.java
+++ 12th - December/x8_memorable_places_demo_nick/java/com/k31/memorableplacestoo/MapsActivity.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -26,6 +27,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -154,7 +156,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locations.add(latLng);
         arrayAdapter.notifyDataSetChanged();
 
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.k31.memorableplacestoo", Context.MODE_PRIVATE);
+        try {
+            ArrayList<String> latitudes = new ArrayList<>();
+            ArrayList<String> longitudes = new ArrayList<>();
+
+            for (LatLng coord : locations) {
+                latitudes.add(Double.toString(coord.latitude));
+                longitudes.add(Double.toString(coord.longitude));
+
+            }
+
+            sharedPreferences.edit().putString("places", ObjectSerializer.serialize(places)).apply();
+            sharedPreferences.edit().putString("lats", ObjectSerializer.serialize(latitudes)).apply();
+            sharedPreferences.edit().putString("longs", ObjectSerializer.serialize(longitudes)).apply();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
         Toast.makeText(this, "Location Saved!", Toast.LENGTH_SHORT).show();
+
+
+//
+
 
     }
 }
