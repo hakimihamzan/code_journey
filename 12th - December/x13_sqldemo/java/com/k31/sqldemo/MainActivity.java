2d6d7c99ed4f82f692d3036f8ae1c63d004389cc
hakimihamzan <hakimihamzan20@gmail.com>
2020-12-10 23:46:23 +0800

    adv sqlite feature

--- 12th - December/x13_sqldemo/java/com/k31/sqldemo/MainActivity.java
+++ 12th - December/x13_sqldemo/java/com/k31/sqldemo/MainActivity.java
@@ -14,57 +14,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
-//        //create table users
-//        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARVCHAR, age INT(3)) ");
-//        //insert into the row values
-//        //myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Nick', 28)");
-//        //myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Kimi', 27)");
-//
-//
-//        // take somethin out
-//        Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
-//
-//        // get index to access
-//        int nameIndex = c.getColumnIndex("name");
-//        int ageIndex = c.getColumnIndex("age");
-//
-//
-//        // move to top to process
-//        c.moveToFirst();
-//
-//
-//        while (!c.isAfterLast()) {
-//            Log.i("name", c.getString(nameIndex));
-//            Log.i("age", c.getString(ageIndex));
-//
-//            c.moveToNext();
-//        }
+        SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+        //create table users
+        myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARVCHAR, age INT(3)) ");
+        //insert into the row values
+        //myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Nick', 28)");
+        //myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Kimi', 27)");
 
 
-        SQLiteDatabase myEventDB = this.openOrCreateDatabase("events", MODE_PRIVATE, null);
-        myEventDB.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
+        // take somethin out
+        Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
 
-        myEventDB.execSQL("INSERT INTO events (name, year) VALUES ('Convovation', 2019)");
-        myEventDB.execSQL("INSERT INTO events (name, year) VALUES ('COVID party', 2020)");
-        myEventDB.execSQL("INSERT INTO events (name, year) VALUES ('House warming party', 2021)");
+        // get index to access
+        int nameIndex = c.getColumnIndex("name");
+        int ageIndex = c.getColumnIndex("age");
 
-        Cursor c = myEventDB.rawQuery("SELECT * FROM events", null);
 
+        // move to top to process
         c.moveToFirst();
 
-        int nameIndex = c.getColumnIndex("name");
-        int yearIndex = c.getColumnIndex("year");
 
         while (!c.isAfterLast()) {
-            Log.i("name-------", c.getString(nameIndex));
-            Log.i("year-------", c.getString(yearIndex));
+            Log.i("name", c.getString(nameIndex));
+            Log.i("age", c.getString(ageIndex));
 
             c.moveToNext();
         }
 
-        //after use close cursor
-        c.close();
+
+//        SQLiteDatabase myEventDB = this.openOrCreateDatabase("events", MODE_PRIVATE, null);
+//        myEventDB.execSQL("CREATE TABLE IF NOT EXISTS events (name VARCHAR, year INT(4))");
+//
+//        myEventDB.execSQL("INSERT INTO events (name, year) VALUES ('Convovation', 2019)");
+//        myEventDB.execSQL("INSERT INTO events (name, year) VALUES ('COVID party', 2020)");
+//        myEventDB.execSQL("INSERT INTO events (name, year) VALUES ('House warming party', 2021)");
+//
+//        Cursor c = myEventDB.rawQuery("SELECT * FROM events", null);
+//
+//        c.moveToFirst();
+//
+//        int nameIndex = c.getColumnIndex("name");
+//        int yearIndex = c.getColumnIndex("year");
+//
+//        while (!c.isAfterLast()) {
+//            Log.i("name-------", c.getString(nameIndex));
+//            Log.i("year-------", c.getString(yearIndex));
+//
+//            c.moveToNext();
+//        }
+//
+//        //after use close cursor
+//        c.close();
 
 
 
