48f47c9a2bf2e773e3241e77a6f6db95de6637b5
hakimihamzan <hakimihamzan20@gmail.com>
2020-12-13 16:08:50 +0800

    update showing users list second screen

--- 12th - December/x17_insta_clone_nick/java/com/example/instragramclone/MainActivity.java
+++ 12th - December/x17_insta_clone_nick/java/com/example/instragramclone/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.instragramclone;
 // NICK
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,15 +22,20 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener
 {
     Boolean signUpModeActive = true;
     TextView loginTextView;
+    EditText usernameEditText;
+    EditText passwordEditText;
+
+    public void showUsersList() {
+        Intent intent = new Intent(getApplicationContext(), UserListActivity.class);
+        startActivity(intent);
+    }
 
 
     public void signUpClicked(View view) {
-        EditText usernameEditText = findViewById(R.id.usernameEditText);
-        EditText passwordEditText = findViewById(R.id.passwordEditText);
 
         if (usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")) {
             Toast.makeText(this, "Username and password required", Toast.LENGTH_SHORT).show();
@@ -41,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void done(ParseException e) {
                         if (e == null) {
                             System.out.println("----------------------------success");
+                            showUsersList();
                         } else {
                             Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show(); // getmessage friendlier
                         }
@@ -52,6 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void done(ParseUser user, ParseException e) {
                         if ( user != null) {
                             System.out.println("--------------log in ok");
+                            showUsersList();
                         } else {
                             Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show(); // getmessage friendlier
 
@@ -72,12 +84,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         loginTextView = (TextView) findViewById(R.id.loginTextView);
+        //this was implement up top - method generated is on click
         loginTextView.setOnClickListener(this);
 
 
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        //this was implement up top - method generated is onkey
+        passwordEditText.setOnKeyListener(this);
 
+        if (ParseUser.getCurrentUser() != null) {
+            showUsersList();
+        }
 
-    
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+        ConstraintLayout backgroundLayout = findViewById(R.id.background_layout);
+
+        logoImageView.setOnClickListener(this);
+        backgroundLayout.setOnClickListener((this));
+
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     
     }
@@ -97,6 +122,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 signUpButton.setText("sign up");
                 loginTextView.setText("or, Log in");
             }
+        } else if (v.getId() == R.id.logoImageView || v.getId() == R.id.background_layout) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         }
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+            signUpClicked(v);
+        }
+
+        return false;
     }
 }
---
+++ 12th - December/x17_insta_clone_nick/java/com/example/instragramclone/UserListActivity.java
@@ -0,0 +1,58 @@
+package com.example.instragramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayList<String> usernames = new ArrayList<>();
+
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, usernames);
+
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+        // alphabetical
+        query.addAscendingOrder("username");
+
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e == null) {
+                    if (objects.size() > 0) {
+                        for (ParseUser user : objects) {
+                            usernames.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+
+                    } else {
+                        e.printStackTrace();
+
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+    }
+}
